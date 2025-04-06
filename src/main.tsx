fe43bde446c4f85d2e279ddfeea127ce86a768e9
Josh Smithson <josh@cutmy.co.uk>
2025-04-06 15:06:55 +0100

    build settings

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-); 
+// Get the root element
+const root = document.getElementById('root');
+
+// Create and render the root
+if (root) {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+} 
